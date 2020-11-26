208bbdf22c08f9df764091eb98bb313e9e63e4ad
fange518 <60872842+fange518@users.noreply.github.com>
2020-11-26 22:58:14 +0800

    javabook

---
+++ src/Example2/Example2_1.java
@@ -0,0 +1,16 @@
+package Example2;
+//用类型转换显示一些字符在Unicode表中的位置
+
+public class Example2_1 {
+    public static void main(String[] args) {
+        char chinaWord ='好',japanWordd = '$';
+        char you = '\u4F60';
+        int position = 20320;
+        System.out.println("汉字："+chinaWord+"的位置："+(int)chinaWord);
+        System.out.println(position +"位置上的字符是："+(char)position);
+        System.out.println("you:"+you);
+
+
+    }
+
+}
---
+++ src/Example3/Example3_4.java
@@ -0,0 +1,4 @@
+package Example3;
+
+public class Example3_4 {
+}
